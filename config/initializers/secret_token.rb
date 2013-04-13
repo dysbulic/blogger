2a658e260e9307bb9dbc947a969f0c391237389d
Will Holcomb <wholcomb@synaptian.com>
2013-04-13 10:43:52 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'c554329968dd9de0ecf5632222dafb440dddecb860a60e770ce342740371a8fdf1cb598c38b50137048492238001cdba4f7c30501dcd5ff8f7fbc66e11f3325d'
